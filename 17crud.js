64f64ccd513abb58560041ba1566e982779a016e
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-04-25 06:37:11 +0530

    Add files via upload

---
+++ 17crud.js
@@ -0,0 +1,12 @@
+//push()  //for adding element in backSide
+
+// const arr=['elon','steve','bill','cr7','aman dhattarwal'];
+// arr.push('heel','dipendra');
+// console.log(arr);
+
+
+//unshift()  //for adding element in front or starting
+
+const arr=['elon','aman','steve','bill','warren'];
+arr.unshift('heel','dipendra');
+console.log(arr);
